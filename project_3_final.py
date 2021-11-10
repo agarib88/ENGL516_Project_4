9b65dc973e0da1c0a61aa64bbc2543d271ca798d
shuhyin <89762819+shuhyin@users.noreply.github.com>
2021-11-10 10:11:30 -0600

    Update project_3_final.py

--- project_3_final.py
+++ project_3_final.py
@@ -1,22 +1,16 @@
 # open a file 
-with open('project_3_example_text_new.txt') as project_3_example_doc:
-    project_3_example_content = project_3_example_doc.read()
-print(project_3_example_content)
+file = input('please input file as .txt')
+with open(file) as file_doc:
+    file_content = file_doc.read()
+print(file_content)
 
 # find the annotation 
 import re
-project_3_annotation = re.findall(r"\[\[.+?\]\]\(\(.+?\)\)", project_3_example_content)
-print(project_3_annotation)
+file_annotation = re.findall(r"\[\[.+?\]\]\(\(.+?\)\)", file_content)
+print(file_annotation)
 
 # save the output as new lines (optimal version)
-with open('trial.txt', "a+") as f:
-    for item in project_3_annotation:
+with open('output.txt', "a+") as f:
+    for item in file_annotation:
         f.write(str(item + '\n'))
 
-# format into different lines (another way for output)
-project_3_annotation_lines = '\n'.join(project_3_annotation)
-print(project_3_annotation_lines)
-
-# save the output
-with open('output.txt', 'w') as output_doc:
-    output_doc.write(project_3_annotation_lines)
